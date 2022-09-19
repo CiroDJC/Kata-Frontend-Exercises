7b19cdc34715c6445d5d7e85ba232fd688fe4238
Ciro <ciro.djc@gmail.com>
2022-09-19 18:28:28 -0500

    add linter exercise

---
+++ linter-project/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es2021': true
+	},
+	// 'extends': 'airbnb-base/legacy',
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
